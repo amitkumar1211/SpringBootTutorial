2736abbefc52bf09287bda579c3d67189a4cb9b7
amit <amitkumar1211@gmail.com>
2021-10-19 15:35:31 +0530

    Signed-off-by: amit <amitkumar1211@gmail.com>

---
+++ springboot-mvc/embedded-server-example/src/main/java/com/amit/sample/EmbeddedServerExampleApplication.java
@@ -0,0 +1,13 @@
+package com.amit.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmbeddedServerExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmbeddedServerExampleApplication.class, args);
+	}
+
+}
