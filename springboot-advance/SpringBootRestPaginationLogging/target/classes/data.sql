2c6b10b804c37d30a52d24de4fb8fece28c3905f
amitkumar1211 <30998441+amitkumar1211@users.noreply.github.com>
2021-10-27 00:24:10 +0530

    committed to new branch
    committed to new branch

---
+++ springboot-advance/SpringBootRestPaginationLogging/target/classes/data.sql
@@ -0,0 +1,16 @@
+INSERT INTO 
+	TBL_EMPLOYEES (first_name, last_name, email) 
+VALUES
+	('Amit', 'Kumar', 'amit@gmail.com'),
+	('Sumit', 'Kumar', 'sumit@gmail.com'),
+	('Rohit', 'Singh', 'rohit@gmail.com'),
+	('Gaurav', 'Kumar', 'gaurav@gmail.com'),
+	('Upendar', 'Yadav', 'upendar@gmail.com'),
+	('Tarun', 'Gupta', 'tarun@gmail.com'),
+	('Madhu', 'Kumari', 'madhu@gmail.com'),
+	('Sai', 'Kumar', 'sai@gmail.com'),
+	('Sasikala', 'Kumari', 'sasikala@gmail.com'),
+	('Manoj', 'Kumar', 'manoj@gmail.com'),
+	('Rohan', 'Singh', 'rohan@gmail.com'),
+	('Shyam', 'Singh', 'shyam@gmail.com'),
+	('XYZ', 'xyz', 'xyz@email.com');
