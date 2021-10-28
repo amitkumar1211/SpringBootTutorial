5f88ab4e846ac691a1c6153918eb189491603de5
amitkumar1211 <30998441+amitkumar1211@users.noreply.github.com>
2021-10-28 13:06:44 +0530

    Signed-off-by: amitkumar1211 <30998441+amitkumar1211@users.noreply.github.com>

---
+++ spring-security/spring-security-example/src/main/java/com/amit/example/config/WebSecurityConfig.java
@@ -0,0 +1,30 @@
+package com.amit.example.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Bean
+	public UserDetailsService userDetailsService() {
+		
+		InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+		manager.createUser(User.withDefaultPasswordEncoder().username("amit").password("amit").roles("ADMIN").build());
+		return manager;
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception{
+		
+		http.authorizeRequests().antMatchers("/user").permitAll()
+		.antMatchers("/admin").authenticated()
+		.and().formLogin();
+	}
+
+}
