4713f68221e741ec075faee52e7f737b26f6ac1e
amitkumar1211 <30998441+amitkumar1211@users.noreply.github.com>
2021-10-28 16:08:39 +0530

    Signed-off-by: amitkumar1211 <30998441+amitkumar1211@users.noreply.github.com>

---
+++ spring-security/spring-security-pre-auth-method/src/main/java/com/amit/example/config/WebSecurityConfig.java
@@ -0,0 +1,38 @@
+package com.amit.example.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Bean
+	public UserDetailsService userDetailsService() {
+		
+		InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+		manager.createUser(User.withDefaultPasswordEncoder().username("amit").password("amit").roles("ADMIN").build());
+		manager.createUser(User.withDefaultPasswordEncoder().username("sumit").password("sumit").roles("USER").build());
+		return manager;
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception{
+		
+		http.authorizeRequests().antMatchers("/users").permitAll()
+		.antMatchers("/users/user").hasRole("USER")
+		.antMatchers("/admin").hasRole("ADMIN")
+		.and().formLogin()
+		.and().rememberMe().key("rem-amit-key")
+		.rememberMeParameter("rememberAmit")
+		.rememberMeCookieName("rememberAmit")
+		.tokenValiditySeconds(100);
+	}
+
+}
---
+++ spring-security/spring-security-pre-auth-method/src/main/java/com/amit/example/web/UserController.java
@@ -0,0 +1,23 @@
+package com.amit.example.web;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+	
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	@GetMapping
+	public String greetingAll() {
+		return "hello All";
+	}
+	
+	@GetMapping("/user")
+	public String greetingUser() {
+		return "hello User";
+	}
+
+}
