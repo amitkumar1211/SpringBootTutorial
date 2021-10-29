4bd4d588adf6d25b6bebb81627ab77fb79bb9fa8
amitkumar1211 <30998441+amitkumar1211@users.noreply.github.com>
2021-10-29 16:08:09 +0530

    Signed-off-by: amitkumar1211 <30998441+amitkumar1211@users.noreply.github.com>

---
+++ order-mgmt/spring-cloud-api-gateway/src/main/java/com/amit/example/SpringCloudApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.amit.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudApiGatewayApplication.class, args);
+	}
+
+}
